26101dfd84bad9c1460d38b369d7fdf14ec0ccb9
laurazenc <laurazenc@gmail.com>
2015-07-25 21:49:41 +0200

    Folder setup + signin page

---
+++ head.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../assets/css/main.css"/>
+<link rel="stylesheet" href="../assets/css/fullcalendar.css"/>
+
+
+<script src="../assets/js/Chart.min.js" type="text/javascript"></script>
+<script src="../assets/js/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script src="../assets/js/moment.min.js" type="text/javascript"></script>
+<script src="../assets/js/fullcalendar.js" type="text/javascript"></script>
+<script src="../assets/js/functions.js" type="text/javascript"></script>
+
+
---
+++ signin/signin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>SignIn</title>
+		<?php include "../head.php"	?>
+	</head>
+	<body class="signin">
+		<div id="login">
+			<h3 class="signin-title">SING IN</h3>
+			<div class="ui_form">
+				<input type="text" placeholder="Username"/>
+			</div>
+			<div class="ui_form">
+				<input type="text" placeholder="Password"/>
+			</div>
+			<span><a href="#">Forgot your password?</a></span>
+			<button>LogIn</button>
+		</div>
+	</body>
+</html>
