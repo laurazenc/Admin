79a5d8b719d1c28303d988bb4debd6e25b43708b
Laura <laurazenc@gmail.com>
2015-07-26 20:44:03 +0200

    Starting profile section

--- header/header.php
+++ header/header.php
@@ -6,14 +6,17 @@
 	<div class="header-container">
 		<div class="user-menu hmenu">
 			<img src="https://s3.amazonaws.com/uifaces/faces/twitter/rogie/128.jpg" alt="" />
-			<div>
+			<div class="user-info">
 				<span class="user-hr">ADMIN</span>
 				<span class="user-name">John Doe</span>
 			</div>
+			<div class="logout">
+				<span><a href="#" alt="Logout"><i class="fa fa-power-off"></i></a></span>
+			</div>
 		</div>
 		<div class="header-menu hmenu">
-			<a href="#" class="header-openmenu"> <!-- MOBILE RESPONSIVE MENU -->
-				<i class="fa fa-bars"></i>
+			<a href="#" class="header-openmenu">
+				<i class="fa fa-bars"></i>MENU
 			</a>
 			
 			<ul class="nav-menu">
@@ -22,9 +25,6 @@
 				<li><a href="#"><i class="fa fa-inbox"><em>0</em></i>NOTIFICATIONS</a></li>
 				<li><a href="#"><i class="fa fa-calendar"></i>EVENTS</a></li>
 			</ul>
-		</div>
-		<div class="logout hmenu">
-			<span><a href="#"><i class="fa fa-power-off"></i></a></span>
 		</div>
 	</div>
 </div>
