d8c8cb84a36283ae7fa8d051a8995932590e9f91
Laura <laurazenc@gmail.com>
2015-07-26 19:24:44 +0200

    Starting header

---
+++ dashboard/dashboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>SignIn</title>
+		<?php include "../head.php"	?>
+	</head>
+	<body class="dashboard">
+		
+	</body>
+</html>
---
+++ header/header.php
@@ -0,0 +1,32 @@
+<!-- DEVELOPMENT ONLY -->
+<?php include "../head.php"	?>
+
+
+<div class="header">
+	<div class="header-container">
+		<div class="user-menu hmenu">
+			<img src="https://s3.amazonaws.com/uifaces/faces/twitter/rogie/128.jpg" alt="" />
+			<div>
+				<span class="user-hr">ADMIN</span>
+				<span class="user-name">John Doe</span>
+			</div>
+		</div>
+		<div class="header-menu hmenu">
+			<a href="#" class="header-openmenu"> <!-- MOBILE RESPONSIVE MENU -->
+				<i class="fa fa-bars"></i>
+			</a>
+			
+			<ul class="nav-menu">
+				<li><a href="#"><i class="fa fa-home"></i>HOME</a></li>
+				<li><a href="#"><i class="fa fa-user"></i>PROFILE</a></li>
+				<li><a href="#"><i class="fa fa-inbox"><em>0</em></i>NOTIFICATIONS</a></li>
+				<li><a href="#"><i class="fa fa-calendar"></i>EVENTS</a></li>
+			</ul>
+		</div>
+		<div class="logout hmenu">
+			<span><a href="#"><i class="fa fa-power-off"></i></a></span>
+		</div>
+	</div>
+</div>
+
+
