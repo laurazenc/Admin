c35ab4a8d5234f978871b90d2e84c837d9dc8566
laurazenc <laurazenc@gmail.com>
2015-07-28 15:56:39 +0200

    'Updated

---
+++ profile/profile.php
@@ -0,0 +1,36 @@
+<!-- DEVELOPMENT ONLY -->
+<?php include "../header/header.php"	?>
+
+<div class="profile-wrapper">
+	<div class="profile-container">
+
+		<div class="profile-info">
+			<div class="profile-main">
+				<div class="profile-img"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/rogie/128.jpg" alt=""></div>
+				<div class="profile-data">
+					<span class="username">John Doe</span>
+					<span class="location">Madrid, Spain</span>
+				</div>
+			</div>
+			<div class="profile-about">
+				<h3>About</h3>
+				<p>Selvage 3 wolf moon Wes Anderson VHS gentrify, roof party blog vinyl polaroid Blue Bottle chambray church-key Bushwick mixtape PBR&B. PBR&B 3 wolf moon Williamsburg.</p>
+			</div>
+		</div>
+
+		<div class="profile-posts-wrap">
+			<ol class="posts">
+				<li class="post">
+					<div class="post-thumb">
+						<img src="" alt=""/>
+					</div>
+					<div class="post-title">
+						Gluten-free stumptown pour-over PBRB YOLO mumblecore.
+					</div>
+					<div class="post-info"></div>
+				</li>	
+			</ol>
+			
+		</div>
+	</div>
+</div>
